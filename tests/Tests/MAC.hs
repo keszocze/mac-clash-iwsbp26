7a49799a6344b4cbcb3bf3e5e789a4d3aff0a438
Oliver Keszöcze <info@oliver.keszoecze.de>
2026-06-26 13:38:15 +0200

    simplify tests for the extended design

--- tests/Tests/MAC.hs
+++ tests/Tests/MAC.hs
@@ -2,21 +2,29 @@ module Tests.MAC where
 
 import Test.Tasty
 
-import Tests.MAC.Extended
+
 
 import Tests.MAC.Simple.Exhaustive as SE
 import Tests.MAC.Simple.Random as SR
+import Tests.MAC.Extended.Exhaustive as EE
+import Tests.MAC.Extended.Random as ER
 
 
--- TODO unify test case generation (see the mkMAC functions)
+-- TODO unify test case generation (see the mkMAC functions) -- halfway done :)
 
 tests :: TestTree
 tests = testGroup "MAC Unit" [
 -- TODO add options for different levels of exhaustiveness / iterations
-    Tests.MAC.Extended.tests,
+    extendedTests,
     simpleTests
   ]
 
+
+extendedTests :: TestTree
+extendedTests = testGroup "Extended version" [
+    testGroup "Exhaustive Tests" EE.singleExhaustiveTest,
+    testGroup "Random Tests" ER.tinyRandomTests
+  ]
 
 simpleTests :: TestTree
 simpleTests =  testGroup "Simple version" [
